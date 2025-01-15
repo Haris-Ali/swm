4028b7e104b4b26158fa4deded82737ac9c92b3a
Haris-Ali <haris.ali@vizteck.com>
2025-01-15 15:11:04 +0000

    Add staff member view

--- src/main/java/org/example/swm/controllers/staff/AddStaffMember.java
+++ src/main/java/org/example/swm/controllers/staff/AddStaffMember.java
@@ -1,54 +1,61 @@
 package org.example.swm.controllers.staff;
 
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import org.example.swm.models.StaffMember;
 import org.example.swm.services.StaffService;
 
-import java.util.Scanner;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * The class Add staff member for adding new staff members.
  */
-public class AddStaffMember {
+public class AddStaffMember implements Initializable {
+    public TextField name_field;
+    public ChoiceBox contract_field;
+    public TextField subject_area_field;
+    public TextField line_manager_field;
+    public Button add_button;
+    public Label error_label;
+    public Label success_label;
     /**
      * The Staff service.
      */
     StaffService staffService = new StaffService();
 
-    /**
-     * Setup function to input staff name, contract type, subject area & line manager
-     */
-    public void setup() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter Staff Name: ");
-        String staffName = sc.nextLine();
-        Double staffContractType = null;
-        while (staffContractType == null) {
-            System.out.print("Enter Staff Contract Type (1.0 for full time & 0.5 for part-time): ");
-            String ctInput = sc.nextLine();
-            if (ctInput.matches("^(1(\\.0)?|0\\.5|\\.5)$")) {
-                staffContractType = Double.parseDouble(ctInput);
-            } else {
-                System.out.println("Invalid input. Please enter 1, 1.0, 0.5, or .5");
-            }
-        }
-        System.out.print("Enter Subject Area: ");
-        String staffSubjectArea = sc.nextLine();
-        System.out.print("Enter Line Manager: ");
-        String staffLineManager = sc.nextLine();
-
-        addStaffMember(staffName, staffContractType, staffSubjectArea, staffLineManager);
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        contract_field.getItems().add("Full time");
+        contract_field.getItems().add("Part time");
+        add_button.setOnAction(e -> addStaffMember());
     }
 
     /**
      * Method to add new staff member
-     * @param name the staff member name
-     * @param contractType the contract type (full-time/part-time)
-     * @param subjectArea the subject area
-     * @param lineManager the line manager
      */
-    private void addStaffMember(String name, Double contractType, String subjectArea, String lineManager) {
+    private void addStaffMember() {
+        String name = name_field.getText();
+        String contractValue = (String) contract_field.getValue();
+        String subjectArea = subject_area_field.getText();
+        String lineManager = line_manager_field.getText();
+
+        error_label.setText("");
+        success_label.setText("");
+        if (name.isEmpty() || contractValue.isEmpty() || subjectArea.isEmpty() || lineManager.isEmpty()) {
+            error_label.setText("All fields are required");
+            return;
+        }
+
+        double contractType;
+        if (contractValue.equals("Full time")) contractType = 1.0;
+        else contractType = 0.5;
         StaffMember sm = new StaffMember(name, contractType, subjectArea, lineManager);
         staffService.addStaffMember(sm);
+        error_label.setText("");
+        success_label.setText("Staff member added successfully!");
     }
 }
